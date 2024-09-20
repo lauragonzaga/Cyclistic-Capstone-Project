821a5a15bec3fad67062d582aff906809ad081d0
lauragonzaga <lauracgonzaga@gmail.com>
2024-09-20 15:19:27 -0300

    Update load data infile.sql

--- load data infile.sql
+++ load data infile.sql
@@ -30,32 +30,4 @@ LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
 
-CREATE TABLE data202407
-(
-ride_id VARCHAR(255),
-rideable_type VARCHAR(255),
-started_at DATETIME,
-ended_at DATETIME,
-start_station_name VARCHAR(255),
-start_station_id VARCHAR(255),
-end_station_name VARCHAR(255),
-end_station_id VARCHAR(255),
-start_lat VARCHAR(255),
-start_lng VARCHAR(255),
-end_lat VARCHAR(255),
-end_lng VARCHAR(255),
-member_casual VARCHAR(255)
-);
-
-
-LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/202407-divvy-tripdata.csv"
-INTO TABLE data202407
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
-
-
-select count(*) from data202407
-
-
+-- repeated this process for every table
